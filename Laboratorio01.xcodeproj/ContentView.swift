7eb632109089d4e91a6fb309cedd156bae9a9650
CalepNeyra <calep.neyra@tecsup.edu.pe>
2026-09-01 23:12:29 -0500

    feat: Crear proyecto Lab01 y Playground en Semana01

---
+++ Laboratorio01.xcodeproj/ContentView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Laboratorio 01 - Intro Swift")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Laboratorio01.xcodeproj/Laboratorio01App.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct Laboratorio01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
