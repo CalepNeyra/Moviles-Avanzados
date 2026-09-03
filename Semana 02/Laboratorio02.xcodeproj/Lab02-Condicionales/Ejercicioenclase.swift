a4649da307f73d781bc259b4faed6a24b483a911
CalepNeyra <calep.neyra@tecsup.edu.pe>
2026-09-02 19:09:59 -0500

    feat: Agregar ejercicios en clase de la semana02

---
+++ Semana 02/Laboratorio02.xcodeproj/Lab02-Condicionales/Ejercicioenclase.swift
@@ -0,0 +1,19 @@
+//
+//  Lab02.swift
+//  Laboratorio00
+//
+//  Created by Tecsup on 27/08/26.
+//
+
+import Foundation
+
+import Foundation
+
+print("Numero 1")
+let a = Double(readLine() ?? "") ?? 0
+
+print("Numero 2")
+let b = Double(readLine() ?? "") ?? 0
+
+let suma = a + b
+print("La suma es \(suma)")
