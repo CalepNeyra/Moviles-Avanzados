3c48532dc9522b47d5b8bb14e60b13272de174a7
CalepNeyra <calep.neyra@tecsup.edu.pe>
2026-09-17 21:16:48 -0500

    feat: agregar class libro y la biblioteca

--- Semana 04/ejercicio2A.swift
+++ Semana 04/ejercicio2A.swift
@@ -12,3 +12,59 @@ struct Libro {
     let autor: String
     var estado: EstadoLibro = .disponible
 }
+// 3. class Biblioteca
+class Biblioteca {
+    var libros: [Libro] = []
+    
+    func agregar(libro: Libro) {
+        libros.append(libro)
+    }
+    
+    func prestar(titulo: String) -> Bool {
+        for i in 0..<libros.count {
+            if libros[i].titulo == titulo {
+                if libros[i].estado == .disponible {
+                    libros[i].estado = .prestado
+                    print("Préstamo aprobado: \(titulo)")
+                    return true
+                } else {
+                    print("Error: \(titulo) ya está prestado")
+                    return false
+                }
+            }
+        }
+        print("Error: no existe \(titulo)")
+        return false
+    }
+    
+    func devolver(titulo: String) -> Bool {
+        for i in 0..<libros.count {
+            if libros[i].titulo == titulo {
+                if libros[i].estado == .prestado {
+                    libros[i].estado = .disponible
+                    print("Devolución registrada: \(titulo)")
+                    return true
+                } else {
+                    print("Error: \(titulo) no está prestado")
+                    return false
+                }
+            }
+        }
+        print("Error: no existe \(titulo)")
+        return false
+    }
+    
+    func inventario() {
+        print("===== INVENTARIO =====")
+        for libro in libros {
+            var estadoTexto = ""
+            switch libro.estado {
+            case .disponible:
+                estadoTexto = "disponible"
+            case .prestado:
+                estadoTexto = "prestado"
+            }
+            print("\(libro.titulo) (\(libro.autor)) - \(estadoTexto)")
+        }
+    }
+}
